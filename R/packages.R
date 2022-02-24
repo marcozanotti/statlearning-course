9cceca9fce1bf23f58f9da4295d8adb8541dd66d
Marco Zanotti <marco.zanotti@blogmeter.it>
2022-02-24 11:05:12 +0100

    upload course contents

---
+++ R/packages.R
@@ -0,0 +1,13 @@
+# Packages
+pkgs <- c(
+  "tidyverse", 
+  "lubridate", 
+  "readxl",
+  "tidymodels",
+  "timetk",
+  "modeltime",
+  "performance",
+  "DALEX",
+  "modelStudio"
+)
+install_and_load(pkgs)
